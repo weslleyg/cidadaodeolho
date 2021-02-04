948093d7f1eb41b1db8bffb6472bfb99cfdc485f
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-02-04 20:11:04 -0300

    coletando dados da api externa

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import request from 'request';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+const url = 'http://dadosabertos.almg.gov.br/ws/prestacao_contas/verbas_indenizatorias/deputados/26143/2020/2?formato=json';
+
+request(url, (error, res, body) => {
+    console.log(body);
+});
 
 export { app };
